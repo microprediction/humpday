7744a0363f4d665a86be21e77fa8b81ed964213a
Peter Cotton <peter.cotton@microprediction.com>
2021-02-15 17:14:01 -0500

    ni

--- humpday/comparison/timing.py
+++ humpday/comparison/timing.py
@@ -17,6 +17,9 @@ def get_timing():
     return getjson('https://raw.githubusercontent.com/microprediction/humpday/main/humpday/comparison/timing.json')
 
 
+def exclude(name,n_dim,n_trials):
+    return 'shgo' in name and n_dim>13
+
 
 def create_timing(max_elapsed=5*60):
     """ Add to the timing JSON file """
@@ -27,17 +30,24 @@ def create_timing(max_elapsed=5*60):
     except Exception:
         cpu = dict()
 
-    for opt in OPTIMIZERS:
-        print(opt.__name__)
-        if not cpu.get(opt.__name__):
-            cpu[opt.__name__] = dict()
-        n_failures = 0
-        elapsed = -1
-        for n_dim in [2,3,5,8,13]:
+    for n_dim in [2, 3, 5, 8, 13,21]:
+        print(' ')
+        print('*********** Dimension '+str(n_dim)+' ************')
+        print(' ')
+        for opt in OPTIMIZERS:
+            print(opt.__name__)
+            if not cpu.get(opt.__name__):
+                cpu[opt.__name__] = dict()
+            n_failures = 0
+            elapsed = -1
+
             if not cpu[opt.__name__].get(n_dim):
                 cpu[opt.__name__][n_dim]=dict()
-                print('n_dim='+str(n_dim))
-                for n_trials in [20,30,50,80,130,210,340]:
+                for j,n_trials in enumerate([20,30,50,80,130,210,340,550]):
+                    print(' '*j+str(n_trials))
+                    if exclude(opt.__name__, n_dim, n_trials):
+                        cpu[opt.__name__][n_dim][n_trials] = -1
+
                     if not cpu[opt.__name__][n_dim].get(n_trials):
                         tck = time.time()
                         try:
@@ -57,10 +67,10 @@ def create_timing(max_elapsed=5*60):
                             break
                         if elapsed>max_elapsed:
                             break
-            if n_failures >= 1:
-                break
-            if elapsed > 60 * 10:
-                break
+                    if n_failures >= 1:
+                        break
+                    if elapsed > 60 * 10:
+                        break
 
             with open('timing.json','wt') as fp:
                 json.dump(cpu,fp=fp)
