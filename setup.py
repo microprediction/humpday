55362f61e9918b4a300c43d65cad1f4158cd47f8
Peter Cotton <peter.cotton@microprediction.com>
2021-04-12 14:44:50 -0400

    ver

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="humpday",
-    version="0.3.9",
+    version="0.4.0",
     description="Taking the pain out of choosing a Python global optimizer",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -25,8 +25,8 @@ setup(
     tests_require=['pytest'],
     include_package_data=True,
     install_requires=["wheel", "pathlib", "numpy>=1.19.5", "importlib-metadata>=1.7.0", "getjson",
-                      "scipy", "scikit-optimize", "poap", "pySOT", "funcy", "optuna", "deap", "ax-platform",
-                      "platypus-opt","py-bobyqa","scipy","winning","hebo",
+                      "scipy", "scikit-optimize", "poap", "pySOT", "funcy", "optuna", "deap",
+                      "platypus-opt","scipy","winning","hebo",
                       "pymoo", "nevergrad", "landscapes", "bayesian-optimization", "nlopt","pymorton"],
     entry_points={
         "console_scripts": [
