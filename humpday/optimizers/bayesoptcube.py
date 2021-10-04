0e146a5f7b408c87630b1247bd9c8a6a1cbdfa5b
Peter Cotton <petercotton@Peters-Mac-mini-2.local>
2021-10-04 18:22:44 -0400

    again

--- humpday/optimizers/bayesoptcube.py
+++ humpday/optimizers/bayesoptcube.py
@@ -1,62 +1,71 @@
-from bayes_opt import BayesianOptimization
-from humpday.objectives.classic import CLASSIC_OBJECTIVES
+try:
+    from bayes_opt import BayesianOptimization
+    using_bayesopt = True
+except ImportError:
+    using_bayesopt = False
+
+if using_bayesopt:
+    from humpday.objectives.classic import CLASSIC_OBJECTIVES
 
 
-BAYESOPT_METHODS = ['ucb', 'ei', 'poi']
+    BAYESOPT_METHODS = ['ucb', 'ei', 'poi']
 
-def bayesopt_cube_factory(objective, n_trials, n_dim, with_count,method):
+    def bayesopt_cube_factory(objective, n_trials, n_dim, with_count,method):
 
-    assert method in BAYESOPT_METHODS
+        assert method in BAYESOPT_METHODS
 
-    global feval_count
-    feval_count = 0
-
-    pbounds = dict([('u' + str(i), (0., 1.)) for i in range(n_dim)])
-
-    def _neg_objective(**kwargs) -> float:
         global feval_count
-        feval_count += 1
-        u = [kwargs['u' + str(i)] for i in range(n_dim)]
-        return -objective(u)
+        feval_count = 0
 
-    optimizer = BayesianOptimization(
-        f=_neg_objective,
-        pbounds=pbounds,
-        verbose=0,
-        random_state=1,
-    )
+        pbounds = dict([('u' + str(i), (0., 1.)) for i in range(n_dim)])
 
-    optimizer.maximize(
-        init_points=5,
-        acq=method,
-        n_iter=n_trials - 5,
-    )
+        def _neg_objective(**kwargs) -> float:
+            global feval_count
+            feval_count += 1
+            u = [kwargs['u' + str(i)] for i in range(n_dim)]
+            return -objective(u)
 
-    best_val = -optimizer.max['target']
-    best_x = [optimizer.max['params']['u' + str(i)] for i in range(n_dim)]
+        optimizer = BayesianOptimization(
+            f=_neg_objective,
+            pbounds=pbounds,
+            verbose=0,
+            random_state=1,
+        )
 
-    return (best_val, best_x, feval_count) if with_count else (best_val, best_x)
+        optimizer.maximize(
+            init_points=5,
+            acq=method,
+            n_iter=n_trials - 5,
+        )
 
+        best_val = -optimizer.max['target']
+        best_x = [optimizer.max['params']['u' + str(i)] for i in range(n_dim)]
 
-def bayesopt_ucb_cube(objective, n_trials, n_dim, with_count):
-    return bayesopt_cube_factory(objective=objective,n_trials=n_trials,
-                                 n_dim=n_dim, with_count=with_count,method='ucb')
-
-
-def bayesopt_ei_cube(objective, n_trials, n_dim, with_count):
-    return bayesopt_cube_factory(objective=objective,n_trials=n_trials,
-                                 n_dim=n_dim, with_count=with_count,method='ei')
+        return (best_val, best_x, feval_count) if with_count else (best_val, best_x)
 
 
-def bayesopt_poi_cube(objective, n_trials, n_dim, with_count):
-    return bayesopt_cube_factory(objective=objective,n_trials=n_trials,
-                                 n_dim=n_dim, with_count=with_count,method='poi')
+    def bayesopt_ucb_cube(objective, n_trials, n_dim, with_count):
+        return bayesopt_cube_factory(objective=objective,n_trials=n_trials,
+                                     n_dim=n_dim, with_count=with_count,method='ucb')
 
 
-BAYESOPT_OPTIMIZERS = [ bayesopt_ucb_cube, bayesopt_ei_cube, bayesopt_poi_cube ]
+    def bayesopt_ei_cube(objective, n_trials, n_dim, with_count):
+        return bayesopt_cube_factory(objective=objective,n_trials=n_trials,
+                                     n_dim=n_dim, with_count=with_count,method='ei')
+
+
+    def bayesopt_poi_cube(objective, n_trials, n_dim, with_count):
+        return bayesopt_cube_factory(objective=objective,n_trials=n_trials,
+                                     n_dim=n_dim, with_count=with_count,method='poi')
+
+
+    BAYESOPT_OPTIMIZERS = [ bayesopt_ucb_cube, bayesopt_ei_cube, bayesopt_poi_cube ]
+else:
+    BAYESOPT_OPTIMIZERS = []
 
 
 if __name__=='__main__':
+    assert using_bayesopt
     for objective in CLASSIC_OBJECTIVES:
         print(' ')
         print(objective.__name__)
