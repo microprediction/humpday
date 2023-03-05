7a37dea485685b0fedca83888b0cbe068a42dbd8
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2023-03-04 16:31:27 -1000

    Update plotting.py

--- humpday/visualization/plotting.py
+++ humpday/visualization/plotting.py
@@ -30,3 +30,46 @@ def mesh2d(f,*args):
     ax.set_zlabel('Z Label')
 
     plt.show()
+    
+    
+ def simplex_surf(g):
+    """
+         g is a function defined on the 2-simplex 
+         We change perspective so as to plot it 
+    """
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib as mpl
+    mpl.rcParams['figure.figsize'] = [10.0, 8.0]
+
+    # Define the vertices of the triangle in R3
+    p3 = np.array([0, 0, 0])
+    p1 = np.array([1, 0, 0])
+    p2 = np.array([0.5, np.sqrt(3)/2, 0])
+
+    # Define points in the 2-simplex 
+    s1s = list()
+    s2s = list()
+    s3s = list()
+    for s1 in np.linspace(0,1.0,50):
+      for s2 in np.linspace(0,1.0,50):
+          s3 = 1-s1-s2
+          if 0 <= s3 <= 1:
+              s1s.append(s1)
+              s2s.append(s2)
+              s3s.append(s3)
+
+    X = np.array([ s1*p1[0] + s2*p2[0] + s3*p3[0] for s1, s2, s3 in zip(s1s,s2s,s3s) ])
+    Y = np.array([ s1*p1[1] + s2*p2[1] + s3*p3[1] for s1, s2, s3 in zip(s1s,s2s,s3s) ])
+    Z = np.array([ g(u=[s1,s2,s3]) for s1, s2, s3 in zip(s1s,s2s,s3s)])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    from matplotlib import cm
+    surf = ax.plot_trisurf(X, Y, Z, cmap=cm.coolwarm,
+                          linewidth=0, antialiased=False)
+    shadow = ax.plot_trisurf(X, Y, np.zeros_like(Z), cmap=cm.coolwarm,
+                          linewidth=0, antialiased=False)
+
+    plt.show()
